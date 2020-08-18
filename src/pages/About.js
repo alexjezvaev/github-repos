9b467730a8f36649577bbd326523993fddbbbdf4
Aleksey <60217501+alekseizhezvaev@users.noreply.github.com>
2020-08-18 17:29:16 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React, { useContext, useEffect } from 'react'
+import { AlertContext } from '../context/alert/alertContext'
+
+export const About = () => {
+    const { alert, hide } = useContext(AlertContext)
+
+    useEffect(() => {
+
+        if (alert) hide()
+
+    })
+
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Информация</h1>
+                <p className="lead">Версия приложения: 1.0.0</p>
+            </div>
+        </div>
+    )
+}
